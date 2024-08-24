7635cba98aedc1987556dab0b4bf85f05b3c7cca
deysbabe <adeola_awolesi@yahoo.com>
2024-08-24 18:21:24 +0100

    creating functions

--- kaisen/kaisen/src/adeola/file.py
+++ kaisen/kaisen/src/adeola/file.py
@@ -1,1 +1,11 @@
 print("hello")
+
+#Functon to create parameters and outputs
+def result(a,b): 
+    result = a+b
+    return result
+print(result(10,5))
+
+
+
+#Function that calls another function
